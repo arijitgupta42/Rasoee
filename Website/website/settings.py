b10d0a9ed5e44f5fa654a731f539b43e382f64c0
Arijit Gupta <arijitgupta2000@gmail.com>
2025-08-25 00:56:47 +0100

    Adding support for pythonanywhere

--- Website/website/settings.py
+++ Website/website/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = 'lvzcnd_b5-1g_w53vur=0l1i(c+b25a66#rjl6vi@7sowp&_pw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['rasoee.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['rasoee.herokuapp.com', '127.0.0.1', 'rasoee.pythonanywhere.com']
 
 
 # Application definition
@@ -75,13 +76,17 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default="sqlite:///" + os.path.join(BASE_DIR, "db.sqlite3")
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
